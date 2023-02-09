4326ed548e63a30fb81dc845b61ad25cda2d22df
Mateus Lanuce <mateuslanuce@gmail.com>
2023-02-09 14:51:13 -0300

    feat: :tada: initial commit with configs and images

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="container bg-red-500">
+
+    <section class="mx-auto">
+      <h1 class="text-4xl font-bold">Pokedex</h1>
+      <p class="text-xl">A simple pokedex app built with Vue 3 and Tailwind CSS</p>
+      
+    </section>
+
+  </main>
+</template>
